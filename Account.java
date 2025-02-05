dfd31788632d4e012611ba7734caed9118a65956
shareaom2006 <shareaom06@gmail.com>
2025-02-06 05:20:24 +0530

    Update

---
+++ Account.java
@@ -0,0 +1,24 @@
+public class Account {
+    private String accountNumber;
+    private double balance;
+
+    public Account(String accountNumber, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: $" + amount);
+        } else {
+            System.out.println("Invalid deposit amount");
+        }
